d4f3e7161932060bb270272399fb73e33f71cf31
vaunire <thedexenzoredd@inbox.ru>
2025-05-10 23:33:02 +0300

    add: navbar + carousel temp

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,6 @@ from django import views
 from django.shortcuts import render
 
 class BaseView(views.View):
-    def get(request, *args, **kwargs):
-        context = {
-
-        }
+    def get(self, request, *args, **kwargs):
+        context = {}
         return render(request, 'base.html', context)
